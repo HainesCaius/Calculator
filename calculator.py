d0445584d9a87b845fa4583b7732d2d350e43231
CaiusHaines <caius@authentise.com>
2021-08-23 16:16:45 +0100

    staring off the main part of code for the calculator interface

---
+++ calculator.py
@@ -0,0 +1,10 @@
+from tkinter import*
+
+cal = Tk()
+cal.titlt("Calculator")
+operator = ""
+text_input = StringVar()
+
+txtDisplay = Entry(cal, font=('arial', 20, 'bold'), textvariable=text_Input, bd=30, insertwidth=4, bg='powder blue', justify='right').grid(columnspan=4)
+
+cal.mainloop()
